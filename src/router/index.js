e0256711119dc9a4cf88a20ec2e32d384347f644
Aries Cs <aries0d0f@gmail.com>
2017-11-20 09:57:07 +0800

    Update router rule.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue          from 'vue'
 import Router       from 'vue-router'
 import Home         from '@/view/Home.vue'
 import Login        from '@/view/Login.vue'
+import * as Post    from '@/view/Post'
 
 Vue.use(Router)
 
@@ -11,7 +12,14 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '/posts/new',
+          name: 'New Post',
+          component: Post.New
+        }
+      ]
     },
     {
       path: '/login',
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="home">
     <Navbar></Navbar>
+    <router-view></router-view>
   </div>
 </template>
 
