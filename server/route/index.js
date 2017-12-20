03202aee8867f2bd967785527ad309a4d5dc7aed
Aries Cs <aries0d0f@gmail.com>
2017-12-20 16:20:20 +0800

    Fix file upload.

--- server/route/index.js
+++ server/route/index.js
@@ -12,7 +12,7 @@ route.use('/', (express.static(path.join(__dirname, '/../../dist'))))
 
 route.put('/upload', (req, res) => {
   var file = req.files.file
-  var fileName = `decade_${md5(file.name)}_${file.type.replace('/', '.')}`
+  var fileName = `decade_${md5(file.name)}_${file.mimetype.replace('/', '.')}`
   // console.log(file, fileName)
   file.mv(path.join(__dirname, '/../../upload/', fileName), err => {
     if (err) return res.status(500).json({ result: 0 }, err)
