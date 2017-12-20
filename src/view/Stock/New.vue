a62b2022863bb59d77f3a044a2eb1f7b92c2ec6d
Aries Cs <aries0d0f@gmail.com>
2017-12-20 17:40:44 +0800

    Simgle-Stock upload panel.

--- src/view/Stock/New.vue
+++ src/view/Stock/New.vue
@@ -3,7 +3,7 @@
     <div class="editor">
       <h1>新增商品</h1>
       <div class="editor-container">
-        <input id="title" type="text" placeholder="商品名稱" v-model="stock.title">
+        <input id="title" type="text" placeholder="商品名稱" v-model="stock.name">
         <div class="classBar">
           <ul>
             <li v-for="(types, i) in classType" @click="classSelect(types, i)"><span class="uppercase soli">{{ types }}</span></li>
@@ -11,25 +11,27 @@
         </div>
         <h2>商品圖片</h2>
         <div class="stock-img">
-          <div class="upload-container" v-show="stockImg.length < 4">
-            <input type="file" name="upload" id="upload" accept="image/*" style="display:none" @change="handleFiles">
-            <div class="button" v-if="!image" @click="img()"><font-awesome-icon icon="camera" /><span>選擇圖片</span></div>
+          <div class="upload-container" v-show="stock.img.length < 4">
+            <input type="file" name="upload" id="stock" accept="image/*" style="display:none" @change="handleFiles">
+            <div class="button" v-if="!image" @click="img('stock')"><font-awesome-icon icon="camera" /><span>選擇圖片</span></div>
             <div class="button pre" v-if="image" @click="imgUploadStock()" :style="{ 'background-image': `url(${image})` }"><span>確定上傳</span></div>
           </div>
-          <div class="upload-container" v-for="(img, i) in stockImg">
-            <div class="close" @click="stockImg.splice(i, 1)"><font-awesome-icon icon="times"/></div>
+          <div class="upload-container" v-for="(img, i) in stock.img">
+            <div class="close" @click="stock.img.splice(i, 1)"><font-awesome-icon icon="times"/></div>
             <div class="image" :style="{ background: `url(${img})`}"></div>
           </div>
         </div>
         <h2>商品資料</h2>
-        <textarea name="" rows='5' placeholder="商品簡介"></textarea>
+        <textarea name="" rows='5' id="summary" placeholder="商品簡介"></textarea>
+        <input id="subtitle" type="text" placeholder="售價" v-model="stock.price">
+        <input id="subtitle" type="text" placeholder="銷貨數量" v-model="stock.count">
         <h2>商品描述</h2>
-        <textarea name="" id="first" rows='10' placeholder="文章段落"></textarea>
+        <textarea name="" id="discription" rows='10' placeholder="文章段落"></textarea>
         <div v-for="(html, i) in addHTML" class="blocks">
           <span class="icon" @click="addHTML.splice(i, 1)" v-if="onRemove"><font-awesome-icon icon="minus-circle" /></span><span class="block" :id="`block-${i}`" v-html="html"></span>
         </div>
         <h2>商品規格</h2>
-        <textarea name="" rows='5' placeholder="多行文字"></textarea>
+        <textarea name="" rows='5' id="note" placeholder="多行文字"></textarea>
         <!-- <div v-for="(html, i) in addHTML" class="blocks">
           <span class="icon" @click="addHTML.splice(i, 1)" v-if="onRemove"><font-awesome-icon icon="minus-circle" /></span><span class="block" :id="`block-${i}`" v-html="html"></span>
         </div> -->
@@ -39,7 +41,7 @@
       <div class="upload-container">
         <input type="file" name="upload" id="upload" accept="image/*" style="display:none" @change="handleFiles">
         <img class="preview" :src="image">
-        <div class="button" v-if="!image" @click="img()">上傳圖片</div>
+        <div class="button" v-if="!image" @click="img('upload')">上傳圖片</div>
         <div class="button" v-if="image" @click="imgUpload()">確定</div>
       </div>
     </div>
@@ -72,10 +74,15 @@ export default {
     return {
       classType: ['體驗活動', '肌膚保養', '居家空間', '生活品味', '味蕾饗宴', '時尚藝術'],
       stock: {
-        title: '',
-        category: -1,
-        author: '',
-        content: ''
+        type: 1,
+        comboList: [],
+        img: [],
+        name: '',
+        info: '',
+        count: '',
+        available: false,
+        price: '',
+        seller: []
       },
       tools: {
         subtitle: `<input id="subtitle" type="text" placeholder="副標題">`,
@@ -86,15 +93,20 @@ export default {
       uploadFile: false,
       addIframe: false,
       image: '',
-      stockImg: [],
-      onRemove: false
+      onRemove: false,
+      pre: {
+        type: '',
+        summary: '',
+        discription: '',
+        note: ''
+      }
     }
   },
   computed: {
     ...mapGetters([ 'user' ])
   },
   mounted () {
-    this.stock.author = this.user.uid
+    this.stock.seller.push(this.user.uid)
     document.querySelectorAll('textarea').forEach(e => {
       e.addEventListener('keyup', this.autosize)
     })
@@ -105,15 +117,15 @@ export default {
       el.srcElement.style.cssText = 'height:' + el.srcElement.scrollHeight + 'px'
     },
     classSelect (types, i) {
-      this.stock.category = i
+      this.pre.type = i
       document.querySelectorAll('.classBar>ul>li').forEach(e => {
         e.className = ''
       })
-      document.querySelectorAll('.classBar>ul>li')[this.stock.category].className = 'active'
-      return this.stock.category
+      document.querySelectorAll('.classBar>ul>li')[this.pre.type].className = 'active'
+      return this.pre.type
     },
-    img () {
-      document.querySelector('#upload').click()
+    img (e) {
+      document.querySelector(`#${e}`).click()
     },
     Iframe () {
       this.addHTML.push(`${document.querySelector('input[name="iframe"]').value}`)
@@ -147,12 +159,12 @@ export default {
     },
     imgUploadStock () {
       var data = new FormData()
-      data.append('file', document.querySelector('#upload').files[0])
+      data.append('file', document.querySelector('#stock').files[0])
       this.$http.put('/upload', data, { 'Content-Type': 'multipart/form-data' })
         .then(
           res => {
             if (res.data.result === 0) {
-              this.stockImg.push(res.data.path)
+              this.stock.img.push(res.data.path)
               this.image = ''
               this.uploadFile = false
             }
@@ -160,7 +172,9 @@ export default {
         )
     },
     preview () {
-      this.stock.content = `<p>${document.querySelectorAll('#first')[0].value}</p>`
+      this.pre.summary = document.querySelectorAll('#summary')[0].value
+      this.pre.discription = `<p>${document.querySelectorAll('#discription')[0].value}</p>`
+      this.pre.note = `<p>${document.querySelectorAll('#note')[0].value}</p>`
       var tmp = []
       var blocks = document.querySelectorAll('.block')
       blocks.forEach(e => {
@@ -185,24 +199,16 @@ export default {
         }
       }
       tmp.forEach(e => {
-        this.stock.content += e
+        this.pre.content += e
       })
+      this.stock.info = JSON.stringify(this.pre)
     },
     submit () {
       this.preview()
-      this.$http.post('/api/stock', {
-        title: this.stock.title,
-        content: this.stock.content,
-        author: this.stock.author
-      }).then(
+      this.$http.post('http://60.249.179.125:3001/stock', this.stock).then(
         res => {
           if (res.data.result === 0) {
-            this.$http.put(`/api/stock/${res.data.pid}`, {
-              category: this.stock.category,
-              image: document.querySelectorAll('img')[0] ? document.querySelectorAll('img')[0].src : null
-            })
-              .then()
-            window.open(`http://60.249.179.125/stock/${res.data.pid}`)
+            window.open(`http://60.249.179.125/shop/${res.data.sid}`)
           } else {
             console.log(res.data)
           }
