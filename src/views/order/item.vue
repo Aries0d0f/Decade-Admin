1453c5cddcb70c8e759f94967289cf0b72be4a3d
IanLeong3712 <ianleong3712@gmail.com>
2018-02-26 16:23:29 +0800

    feat: 新增訂單輸入運單號功能

--- src/views/order/item.vue
+++ src/views/order/item.vue
@@ -44,8 +44,8 @@
               <el-col :span="20" class="table-content">{{filterStatus(data.status)}}</el-col>
             </el-row>
             <el-row class="table-row">
-              <el-col :span="4" class="table-label">貨運商/運單號</el-col>
-              <el-col :span="20" class="table-content"></el-col>
+              <el-col :span="4" class="table-label">運單號</el-col>
+              <el-col :span="20" class="table-content">{{data.logisticCode}}</el-col>
             </el-row>
           </el-card>
         </div>
@@ -56,7 +56,6 @@
             <el-table :data="stocks" v-if="stocks.length > 0" stripe style="width: 100%" header-row-style="background-color: #ebeef5;">
               <el-table-column align="left" prop="id" label="商品編號" width="220"></el-table-column>
               <el-table-column align="left" prop="name" label="品名"></el-table-column>
-              <!-- <el-table-column align="center" prop="price" label="金額" width="100"></el-table-column> -->
               <el-table-column align="center" prop="count" label="數量" width="100"></el-table-column>
             </el-table>
             <el-table :data="tickets" v-if="tickets.length > 0" stripe style="margin-top:1rem;width: 100%" header-row-style="background-color: #ebeef5;">
@@ -68,8 +67,10 @@
             
             <div class="actions">
               <el-button v-if="data.status === 0" type="info" disabled>等待付款</el-button>
-              <el-button v-else-if="data.status === 1" @click="handleShipping" type="warning">出貨</el-button>
-              <el-button v-else type="info" disabled>已出貨</el-button>
+              <el-button v-if="data.status === 1 && !data.logisticCode" @click="handleSetLogisticCode" type="warning">填寫運單號</el-button>
+              <el-button v-if="data.status === 1 && data.logisticCode" @click="handleSetLogisticCode" type="warning">更改運單號</el-button>
+              <el-button v-if="data.status === 1 && userInfo.role === 0" @click="handleShipping" type="warning">出貨</el-button>
+              <el-button v-if="data.status === 2" type="info" disabled>已出貨</el-button>
             </div>
           </el-card>
         </div>
@@ -81,6 +82,7 @@
 <script>
   import { fetchOrder, updateOrder } from '@/api/order'
   import { fetchStock } from '@/api/stock'
+  import { mapGetters } from 'vuex'
 
   export default {
     name: 'Order-Item',
@@ -101,6 +103,9 @@
     },
     async created() {
       await this.getDate()
+    },
+    computed: {
+      ...mapGetters(['userInfo'])
     },
     filters: {
       LocalePrice(price) {
@@ -133,7 +138,26 @@
         this.data = data
         this.coupons = coupons
       },
+      async handleSetLogisticCode() {
+        try {
+          const { value: logisticCode } = await this.$prompt('請輸入運單號', '提示', {
+            confirmButtonText: '確定',
+            cancelButtonText: '取消',
+            // inputPattern: [0-9],
+            inputErrorMessage: '運單格式有誤！'
+          })
+          await updateOrder(this.data.id, { logisticCode })
+          this.data.logisticCode = logisticCode
+          this.$notify({ title: '成功', message: '運單編號新增成功', type: 'success', duration: 1000 })
+        } catch (error) {
+          console.log(error)
+        }
+      },
       async handleShipping() {
+        if (!this.data.logisticCode) {
+          this.$message.error('請先輸入運單號後在進行出貨')
+          return
+        }
         try {
           await this.$confirm('請確認是否已將商品出貨?', '提示', { confirmButtonText: '確定', cancelButtonText: '取消', type: 'warning' })
           await updateOrder(this.data.id, { status: 2 })
