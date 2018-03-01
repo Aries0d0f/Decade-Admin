dcf68c5a846a2d9f35ad8249e2539048167fb425
IanLeong3712 <ianleong3712@gmail.com>
2018-03-01 14:46:45 +0800

    feat: 新增廣告 - 文章首頁banner

--- src/views/advert/layout/postMainTab.vue
+++ src/views/advert/layout/postMainTab.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="ad-index-tab">
-    <el-card class="box-card" :header="item.title" v-for="(item, i) in adData.main" :key="i">
+    <el-card class="box-card" header="Banner 區塊" :body-style="{ padding: '0px' }">
+      <el-tabs type="border-card">
+        <el-tab-pane label="文章主頁">
+          <el-row :gutter="20">
+            <el-col :span="8">
+              <Upload v-model="adData.main.banner.background" :defaultImg="adData.main.banner.background"></Upload>
+            </el-col>
+            <el-col :span="15">
+              <el-input v-model="adData.main.banner.title" placeholder="標題"></el-input>
+              <el-input v-model="adData.main.banner.btnTitle" placeholder="按鈕名稱"></el-input>
+              <el-input v-model="adData.main.banner.btnLink" placeholder="路由"></el-input>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+    <el-card class="box-card" :header="item.title" v-for="(item, i) in adData.main.middleCard" :key="i">
       <div class="stock-block">
-        <PostSelect v-for="(post, j) in adData.main[i].item" :key="j" v-model="adData.main[i].item[j]" :prop-data="post" style="margin-bottom:1rem;width:45%"></PostSelect>
+        <PostSelect v-for="(post, j) in adData.main.middleCard[i].item" :key="j" v-model="adData.main.middleCard[i].item[j]" :prop-data="post" style="margin-bottom:1rem;width:45%"></PostSelect>
       </div>
     </el-card>
   </div>
@@ -10,6 +26,7 @@
 
 <script>
 import PostSelect from '../components/PostSelect'
+import Upload from '@/components/Upload/singleImage2'
 
 export default {
   props: ['propsData'],
@@ -22,7 +39,8 @@ export default {
     this.adData = Object.assign({}, this.propsData)
   },
   components: {
-    PostSelect
+    PostSelect,
+    Upload
   }
 }
 </script>
